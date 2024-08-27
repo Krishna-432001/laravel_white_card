051cc91663a0265a0ff37aa3a5d816f7d015dbbc
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-27 13:37:36 +0530

    officer and admin login page setup added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('title')
 
-Forget Password
+Home page
 
 @endsection
 
@@ -10,5 +10,35 @@ Forget Password
 
 home page 
 
+<input type="radio" name="login_type" value="officer"> Officer login
+
+<input type="radio" name="login_type" value="admin"> Admin login
+
+<div id="result"></div>
+
+<form action="#" method="post" name="officer_login_form">
+
+    <input type="email" name="email">
+
+    <input type="password" name="password">
+
+    <select name="department">
+
+    <option value="any">select any department</option>
+
+    @foreach($departments as $row)
+    <option value="{{ $row->name }}">{{$row->name}}</option>
+    @endforeach
+    </select>
+
+</form>
+
+<form action="#" method="post" name="admin_login_form">
+
+<input type="email" name="email">
+
+<input type="password" name="password">
+
+</form>
 
 @endsection
