f93d9c255a7399431c652286fc22612433e8dda1
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-27 13:36:42 +0530

    department link added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
+use App\Models\Department;
+
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('frontend/home');
+        $data =[
+            'departments' => Department::all()
+        ];
+
+        return view('frontend/home',$data);
     }
 }
