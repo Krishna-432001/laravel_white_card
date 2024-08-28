2276c4571cfa8cf71d8abeaf4c5257ef9aa8de70
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-28 12:57:59 +0530

    Update web.php
    route added

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,27 @@ Route::post('/admin/login', [AuthController::class, 'authenticate'])->name('admi
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/register', [AuthController::class, 'register'])->name('home.register');
+
+use App\Http\Controllers\OfficerController;
+
+Route::get('/officer/create', [OfficerController::class, 'create'])->name('officer.create');
+
+Route::post('/officers', [OfficerController::class, 'store'])->name('officers.store');
+
+Route::get('/officers', [OfficerController::class, 'index'])->name('officers.index');
+
+
+Route::get('/officer/view', [OfficerController::class, 'view'])->name('officer.view');
+
+use App\Http\Controllers\WhiteCardController;
+
+Route::get('/white_card/create', [WhiteCardController::class, 'create'])->name('white_card.create');
+
+Route::post('/whitecard/store', [WhiteCardController::class, 'store'])->name('whitecard.store');
+
+Route::get('/white_card/view', [WhiteCardController::class, 'view'])->name('white_card.view');
+
+Route::get('/white_card/info', [WhiteCardController::class, 'info'])->name('white_card.info');
+
+Route::get('/white_card/status', [WhiteCardController::class, 'status'])->name('white_card.status');
+
