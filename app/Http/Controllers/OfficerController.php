aaf5e638fbbdc7c349cf6a17d8f1db78e8d9a719
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-28 12:56:34 +0530

    Update OfficerController.php
    store function added

--- app/Http/Controllers/OfficerController.php
+++ app/Http/Controllers/OfficerController.php
@@ -11,6 +11,72 @@ class OfficerController extends Controller
         return view('frontend/officer/create');
     }
 
+    public function store(Request $request)
+    {
+        try 
+        {
+            $request->validate([
+                'email' => 'required',
+        
+                'name' => 'required',
+                'password' => 'required',
+              
+                'mobile_number' => 'required',
+
+                'designation' => 'required',
+
+          
+
+                'department' => 'required'
+
+                
+            ]);
+    
+            // Check if validation fails
+            // if ($validator->fails()) {
+            //     return redirect()->back()->withErrors($validator)->withInput();
+            // }
+    
+            // dd($request);
+    
+            $user = User::create([
+                'name' => $request->input('name'),
+            
+                'email' => $request->input('email'),
+
+                'password' => Hash::make($request->input('password')),
+
+                'mobile_number' => $request->input('phone'),
+
+                'designation' => $request->input('address'),
+                
+                'department' => $request->input('department'),
+                
+            ]);
+            
+
+            $selectedRole = Role::where('name', 'Customer')->first();
+
+            // dd($request->input('occupation'));
+            // dd($selectedRole);
+            
+            $user->assignRole($selectedRole->name);
+
+            // Register successful, set success message            
+            $request->session()->flash('success_message', 'User registered successfully');
+    
+            // Redirect or return response
+            return redirect()->route('home.register')->with('success', 'Registration successful!');    
+        } 
+        catch (ValidationException $e) {
+            // return response()->json(['error' => $e->validator->errors()], 200);
+
+            // Validation failed, return validation errors
+            return redirect()->back()->withErrors($e->validator->errors())->withInput();
+            // dump($e->validator->errors());
+        }
+    }
+
     public function view(Request $request)
     {
         return view('frontend/officer/view');
