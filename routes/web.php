e465028f23decda50f2f325c64fa0d2e3b144b2b
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-28 11:00:37 +0530

    Update web.php
    route added to web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,14 @@ Route::get('/', function () {
 use App\Http\Controllers\HomeController;
 
 Route::get('/',[HomeController::class, 'index'])->name('home.index');
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/officer/login', [AuthController::class, 'authenticate'])->name('officer.login');
+
+
+Route::post('/admin/login', [AuthController::class, 'authenticate'])->name('admin.login');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/register', [AuthController::class, 'register'])->name('home.register');
